c40149a42009f96f0be3dce336f6d5c19c53c6cb
Fábio D. Batista <fabio.david.batista@gmail.com>
2012-09-01 23:23:52 -0300

    Melhorado script

--- tcs-install.sh
+++ tcs-install.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
-
-curl -O 'https://raw.github.com/taxweb/tcs-install/master/Makefile'
+#!/bin/bash -e
 
 # Instala Make, se não estiver instalado
-yum -q -y install make
+sudo yum -q -y install make
 
+# Baixa o Makefile para o local correto
+sudo mkdir -p /opt/tcs-install ; cd $_
+sudo curl -s -O 'https://raw.github.com/taxweb/tcs-install/master/Makefile'
+
+# Executa o Makefile com as opções fornecidas na linha de comando
 make $@
